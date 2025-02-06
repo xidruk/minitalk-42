e79ecd5959e14220b8563b4204c86821f6645590
Xidruk <xidruk@gmail.com>
2025-02-06 09:23:06 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,38 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbarkan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/06 09:22:05 by kbarkan           #+#    #+#             */
+/*   Updated: 2025/02/06 09:22:07 by kbarkan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "g_header.h"
 
-t_server *server_struct = NULL;
+t_server	*g_server_struct = NULL;
 
-void leaks_checker()
+int	main(void)
 {
-  system();
-}
+	struct sigaction	sa;
 
-int main()
-{
-    print_data("Generate the server struct ....");
-    server_struct = gen_server_struct();
-    init_server_struct(server_struct);
-    print_data("Init the server struct ...");
-
-    // Set up signal handlers once
-    struct sigaction sa;
-    sa.sa_sigaction = handle_signal;
-    sa.sa_flags = SA_SIGINFO | SA_NODEFER;
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-
-    printf("Server PID: %d\n", getpid());
-
-    while (1)
-    {
-        server_loop(server_struct);
-        if (server_struct->death_state)
-            restart_server_for_next_client(server_struct);
-    }
-
-    clean_up_server(server_struct);
-    print_data("Clean up the server ...");
-    free(server_struct);
-    return (0);
+	print_data("Generate the server struct ....");
+	g_server_struct = gen_server_struct();
+	init_server_struct(g_server_struct);
+	print_data("Init the server struct ...");
+	sa.sa_sigaction = handle_signal;
+	sa.sa_flags = SA_SIGINFO | SA_NODEFER;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	print_formater("Server PID: $[]", getpid());
+	while (1)
+	{
+		server_loop(g_server_struct);
+		if (g_server_struct->death_state)
+			restart_server_for_next_client(g_server_struct);
+	}
+	clean_up_server(g_server_struct);
+	print_data("Clean up the server ...");
+	free(g_server_struct);
+	return (0);
 }
