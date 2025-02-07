218b109f9fb1aa009912a9a1d5e0f5f35a7e817b
Xidruk <xidruk@gmail.com>
2025-02-07 18:16:02 +0100

    Update client.c

--- client.c
+++ client.c
@@ -23,6 +23,14 @@ void	handle_ack(int signal)
 	}
 }
 
+void	client_start_life(t_client *client_struct, int proc_id, char *data)
+{
+	init_client_struct(client_struct, proc_id, data);
+	print_data("Init The Client Struct ...");
+	send_data(client_struct);
+	print_data("Send Data To The Server ...");
+}
+
 void	kill_client_life_cycle(t_client *client_struct, int proc_id)
 {
 	print_data("Send Death Signal To The Server ...");
@@ -39,6 +47,8 @@ int	main(int argc, char **argv)
 	struct sigaction	sa;
 	t_client			*client_struct;
 
+	signal(SIGINT, SIG_IGN);
+	signal(SIGTERM, SIG_IGN);
 	pars_data(argc, argv);
 	proc_id = swap_types(argv[1]);
 	data = argv[2];
@@ -46,15 +56,12 @@ int	main(int argc, char **argv)
 	sa.sa_flags = 0;
 	sigaction(SIGUSR1, &sa, NULL);
 	client_struct = gen_client_struct();
-	init_client_struct(client_struct, proc_id, data);
-	print_data("Init The Client Struct ...");
-	send_data(client_struct);
-	print_data("Send Data To The Server ...");
+	client_start_life(client_struct, proc_id, data);
 	if (mark_finale_signal(client_struct))
 		kill_client_life_cycle(client_struct, proc_id);
 	else
 	{
-		printf("Data was not sent successfully. An error occurred.\n");
+		print_data("Data was not sent successfully. An error occurred.");
 		exit(1);
 	}
 	free(client_struct);
