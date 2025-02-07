9b46fc07c45a46b9afe840f9fa2b5d212daf35f6
Xidruk <xidruk@gmail.com>
2025-02-07 18:12:52 +0100

    Update loop_server.c

--- server_utils/loop_server.c
+++ server_utils/loop_server.c
@@ -17,5 +17,18 @@ extern t_server	*g_server_struct;
 void	server_loop(t_server *g_server_struct)
 {
 	while (!g_server_struct->final_sgs)
-		pause();
+	{
+		usleep(100000);
+		if (g_server_struct->client_proc_id != 0)
+		{
+			if (kill(g_server_struct->client_proc_id, 0) == -1)
+			{
+				print_formater("Client Diad Unexpectedly PID $[] ",
+					g_server_struct->client_proc_id);
+				g_server_struct->is_client_dead = 1;
+				g_server_struct->final_sgs = 1;
+				break ;
+			}
+		}
+	}
 }
