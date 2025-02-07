ae0df361bd858012d2b5b83637676d368176f6ef
Xidruk <xidruk@gmail.com>
2025-02-07 18:13:20 +0100

    Update restart_server.c

--- server_utils/restart_server.c
+++ server_utils/restart_server.c
@@ -16,8 +16,14 @@ extern t_server	*g_server_struct;
 
 void	restart_server_for_next_client(t_server *g_server_struct)
 {
-	print_data("\nRestarting Server For Next Client !");
+	if (g_server_struct->is_client_dead)
+	{
+		print_error("Client died mid-transmission !");
+		print_error("Destroy Server Side !");
+		exit(1);
+	}
+	print_data("Clean Up Server ...");
 	clean_up_server(g_server_struct);
 	init_server_struct(g_server_struct);
-	print_data("Server Ready For New Conection !");
+	print_data("Restart Server For New Connection ...");
 }
